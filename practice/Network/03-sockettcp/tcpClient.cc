ab7abb652065a35d7accff356771c150c58e5ddd
kyrie-leon <gaomeng9518@163.com>
2021-05-20 17:34:54 +0800

    单进程的TCP网络程序

---
+++ practice/Network/03-sockettcp/tcpClient.cc
@@ -0,0 +1,23 @@
+#include"tcpClient.hpp"
+
+void Usage(std::string proc)
+{
+  std::cout << "Usage: " << std::endl;
+  std::cout << '\t' << proc << " " << "ser_ip ser_port" << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+  if(argc != 3)
+  {
+    Usage(argv[0]);
+    exit(1);
+  }
+  tcpClient *tc = new tcpClient(argv[1],atoi(argv[2]));
+
+  tc->initClient();
+  tc->start();
+
+  delete tc;
+  return 0;
+}
